7e632e2842e70da0271134a257ff73d1e857d3de
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2022-08-24 18:41:24 +0800

    fix docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION=1.0.1
+VERSION=1.0.2
 
-docker build -t bluet/vback .
+docker build --pull -t bluet/vback .
 docker scan bluet/vback:latest
 
 docker tag bluet/vback:latest bluet/vback:${VERSION}
@@ -18,7 +18,7 @@ docker buildx create --use
 while true; do
         read -p "Have I Updated VERSION Info? (Is current VERSION=${VERSION} ?) [y/N]" yn
         case $yn in
-                [Yy]* ) docker buildx build -t bluet/vback:latest -t bluet/vback:${VERSION} --platform linux/amd64,linux/arm64/v8,linux/arm64,linux/riscv64,linux/ppc64le,linux/s390x,linux/386,linux/mips64le,linux/mips64,linux/arm/v7,linux/arm/v6 --push .; break;;
+                [Yy]* ) docker buildx build -t bluet/vback:latest -t bluet/vback:${VERSION} --platform linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x,linux/riscv64 --push .; break;;
                 [Nn]* ) exit;;
                 * ) echo "";;
         esac
